477a5e8365c010b6aa791f3f9003e32d86c62ce5
Alexandra G. A. <143737426+LexiGrace@users.noreply.github.com>
2024-05-05 16:35:07 -0600

    try again

--- src/constants/actionTypes.js
+++
@@ -1,9 +0,0 @@
-const Constants = {
-    USER_LOGGEDIN: 'USER_LOGGEDIN',
-    USER_LOGOUT: 'USER_LOGOUT',
-    FETCH_MOVIES: 'FETCH_MOVIES',
-    FETCH_MOVIE: 'FETCH_MOVIE',
-    SET_MOVIE: 'SET_MOVIE'
-}
-
-export default Constants;
